b6e54d921f1275ece936e26b5e85061cdb713e38
Yashika Bihani <yashikabihani7@gmail.com>
2020-09-18 08:46:33 +0530

    Basic Setup for Merge Sort

---
+++ src/Sorting Visualiser/SortingVisualiser.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SortingVisualiser.css'
+
+export default  class SortingVisualiser extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            array :[],
+        };
+    }
+}
